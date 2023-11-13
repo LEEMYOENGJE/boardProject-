d1221d251099b3db1badde380c40ebd1e78e50b6
LEEMYOENGJE <djroehd@naver.com>
2023-11-13 14:20:16 +0900

    모바일 pc 화면

--- src/main/java/project/commons/Utils.java
+++ src/main/java/project/commons/Utils.java
@@ -1,14 +1,19 @@
-package project.commons;
+package org.koreait.commons;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 @Component
+@RequiredArgsConstructor
 public class Utils {
     private static ResourceBundle validationsBundle;
     private static ResourceBundle errorsBundle;
+
+    private final HttpServletRequest request;
 
     static {
         validationsBundle = ResourceBundle.getBundle("messages.validations");
@@ -17,12 +22,18 @@ public class Utils {
 
     public static String getMessage(String code, String bundleType) {
         bundleType = Objects.requireNonNullElse(bundleType, "validation");
-        ResourceBundle bundle = bundleType.equals("error")?errorsBundle:validationsBundle;
-
+        ResourceBundle bundle = bundleType.equals("error")? errorsBundle:validationsBundle;
         try {
             return bundle.getString(code);
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public boolean isMobile() {
+        // 요청 헤더 User-Agent
+        boolean isMobile = request.getHeader("User-Agent").matches(".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*");
+
+        return isMobile;
     }
 }
