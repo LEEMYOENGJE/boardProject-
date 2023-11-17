c774d42fc3c7ea589f34d54ba1831345b6d02c20
LEEMYOENGJE <djroehd@naver.com>
2023-11-17 14:02:58 +0900

    jda 연습

---
+++ src/main/java/project/entities/BoardView.java
@@ -0,0 +1,19 @@
+package project.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.Data;
+
+@Data
+@Entity
+@IdClass(BoardView.class)
+public class BoardView {
+    @Id
+    private Long seq;
+
+    @Id
+    @Column(name="_uid")
+    private Integer uid;
+}
--- src/main/java/project/entities/Member.java
+++ src/main/java/project/entities/Member.java
@@ -10,33 +10,45 @@ import org.hibernate.annotations.UpdateTimestamp;
 import project.commons.constants.MemberType;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @Builder
 @Entity
 @NoArgsConstructor @AllArgsConstructor
 @Table(indexes = {
-        @Index(name="idx_member_userNm", columnList = "userNm"),
-        @Index(name="idx_member_mobile", columnList = "mobile")
+        @Index(name = "idx_member_userNm", columnList = "userNm"),
+        @Index(name = "idx_member_mobile", columnList = "mobile")
 })
-public class Member {
+public class Member extends Base{
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long userNo;
+    private Long UserNo;
 
+    @Column(unique = true, nullable = false, length = 65)
     private String email;
 
+    @Column(name="pw", length = 65, nullable = false)
     private String password;
 
+    @Column(nullable = false, length = 45)
     private String userNm;
 
+    @Column(length = 11)
     private String mobile;
 
+    @Column(length = 10, nullable = false)
+    @Enumerated(EnumType.STRING)
     private MemberType mtype = MemberType.USER;
 
-    @CreationTimestamp
-    private LocalDateTime regDt;
 
-    @UpdateTimestamp
-    private LocalDateTime modDt;
+
+    /*
+    @Transient
+    private String tmpData;
+
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    */
 }
